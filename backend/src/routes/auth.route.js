54f5d56d4ca6676408d200292fa48e4e55f3ab0d
iraajp <raajpatkar@gmail.com>
2025-01-31 03:12:12 +0530

    first commit

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
+const router = express.Router();
+router.post("/signup", signup);
+router.get("/login", login);
+router.get("/logout", logout);
+router.put("/update-profile",protectRoute, updateProfile);
+export default router;
