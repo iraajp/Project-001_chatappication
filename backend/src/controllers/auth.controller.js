06c4587090e9b12139fca6a3300efc57fa360c56
iraajp <raajpatkar@gmail.com>
2025-01-31 03:51:33 +0530

    rofilepic_feeature update

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -77,7 +77,7 @@ export const logout = (req, res) => {
 
 export const updateProfile = async (req, res) => {
   try {
-    const {profilePic} = req.body.
+    const {profilePic} = req.body;
     const userId= req.user._id;
     if(!profilePic){
       return res.status(400).json({message: "profilePic is required"});
